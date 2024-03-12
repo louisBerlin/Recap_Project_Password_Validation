0b3cd59da3e30b4b6c117845077426d4b9602512
louisBerlin <louis.froch@gmail.com>
2024-03-12 14:18:07 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+    static public boolean checkPasswordValidation( String password) {
+
+
+        
+        return false;
+    }
+}
